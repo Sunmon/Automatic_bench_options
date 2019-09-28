b77b83acabb4d3ee01347c3cc0dc3d18afb63e8b
sunmon <blackmilk274@gmail.com>
2019-09-28 22:04:48 +0900

    명령어 입력받아서 터미널에서 돌리는 기능 추가

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
-    cout << "hello world!" << endl;
+    cout << "Input command to run!" << endl;
+    string command;
+    cin >> command;
+
+    const char *c = command.c_str();    //str을 char*로 변환
+
+    system(c);
+    return 0;
+
 }
