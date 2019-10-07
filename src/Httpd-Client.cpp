9f492ba162faa8591e25dcb8bc94312eabb79f13
sunmon <blackmilk274@gmail.com>
2019-10-08 02:56:26 +0900

    Add toto

--- src/Httpd-Client.cpp
+++ src/Httpd-Client.cpp
@@ -15,7 +15,6 @@ Httpd_Client::~Httpd_Client()
 void Httpd_Client::runBenchTool(int cpu, int period, int quota)
 {
     //TODO: env.json에 저장해두고 알아서 jmeter나 hpl dat같은거 위치 꺼내올수있게하자.
-
     string s_cpu = CPUSET[cpu];
     string s_period = to_string(period/1000);
     string s_quota = to_string(quota/1000);
@@ -23,6 +22,7 @@ void Httpd_Client::runBenchTool(int cpu, int period, int quota)
     string jmeter = "~/Desktop/apache-jmeter-5.1.1/bin/jmeter";
     string jmx = "../data/jmeter-setup.jmx";
     
+//TODO: jmeter 실행할때 주소 파라미터로 넘겨서 설정하는거..
     string runJmeter =  jmeter + " -n -t " + jmx + "-JHTTPSampler.domain=" + servAddr + " -l " + outDir + 
                         "cpus" + s_cpu + "_per" + s_period + "_quo" + s_quota + ".csv";
     command(runJmeter);
