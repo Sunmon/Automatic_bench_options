fc890b2dcc8c05366ab64e50a0be99b71f955f9b
sunmon <blackmilk274@gmail.com>
2019-10-10 00:46:23 +0900

    Bug occured

--- src/Bench.cpp
+++ src/Bench.cpp
@@ -8,12 +8,12 @@ using namespace std;
 void Bench::benchmark()
 {
     init();
-    cout << "name: " << this->NAME << " img: " << this->IMG << endl;
-    // runContainer();
-    // cpEnvToContainer();
+    // cout << "name: " << this->NAME << " img: " << this->IMG << endl;
+    runContainer();
+    cpEnvToContainer();
 
     //FIXME: 테스트 후 cpu개수, period 정하기
-    //update container & run benchmark
+    // update container & run benchmark
     // for(int cpu = 0; cpu < CORE; cpu++)
     // {
         // for(int period = 100000; period <= 1000000; period += 100000)
@@ -25,14 +25,14 @@ void Bench::benchmark()
             // for(int period = 1000000; period <= 1000000; period += 1000000)
             // {
 
-                // int cpu = 1; int period = 100000;
-            // updateContainer(cpu, period, period/2);
-            // runBenchTool(cpu, period, period/2);
-            // saveRslt(cpu, period, period/2);
+                int cpu = 1; int period = 100000;
+            updateContainer(cpu, period, period/2);
+            runBenchTool(cpu, period, period/2);
+            saveRslt(cpu, period, period/2);
         // }
     // }
 
-    // stopContainer();
+    stopContainer();
 }    
 
 
